c6db1efe939c54ee5e153318807bd9a4891898c7
Pitchayut Chitsinpchayakun <pitchayut@oxygenai.com>
2019-11-28 14:06:46 +0700

    Updates

---
+++ precache-manifest.c416abdc09c496080565124a6edf5226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf0ba3a4490d58ac7fe86bb9c817d41",
+    "url": "/greenvisible-cloak-web/index.html"
+  },
+  {
+    "revision": "72a8bf379de24881393f",
+    "url": "/greenvisible-cloak-web/static/css/main.01e8a8c1.chunk.css"
+  },
+  {
+    "revision": "a67af5c8efd819abd2a7",
+    "url": "/greenvisible-cloak-web/static/js/2.49af33cd.chunk.js"
+  },
+  {
+    "revision": "72a8bf379de24881393f",
+    "url": "/greenvisible-cloak-web/static/js/main.c2176e37.chunk.js"
+  },
+  {
+    "revision": "c2e2350428d552ddd0ed",
+    "url": "/greenvisible-cloak-web/static/js/runtime-main.a3c9a922.js"
+  },
+  {
+    "revision": "fe2b12b0a27aaf0d2ef5bb14451308c2",
+    "url": "/greenvisible-cloak-web/static/media/HARRP___.fe2b12b0.woff"
+  }
+]);
